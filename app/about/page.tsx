969869c81942835132f8303aa7d43292b4efc431
dev-medina29 <mmbedi@byupathway.edu>
2026-07-06 15:59:31 +0100

    Save

---
+++ app/about/page.tsx
@@ -0,0 +1,39 @@
+interface SkillsProps {
+  langages: string[];
+  contact: string;
+  education: string;
+}
+function Skills({ langages, contact, education }: SkillsProps) {
+  return (
+    <>
+      <h2 className="text-3xl font-bold mb-4">Education : </h2>
+      <p className="text-lg text-gray-700">{education}</p>
+      <h2 className="text-3xl font-bold mb-4">Langages :</h2>
+      <ul>
+        {langages.map((langage) => (
+          <li key={langage} className="text-lg text-gray-700">
+            {langage}
+          </li>
+        ))}
+      </ul>
+      <h2 className="text-3xl font-bold mb-4">Contact :</h2>
+      <p className="text-lg text-gray-700">{contact}</p>
+    </>
+  );
+}
+
+export default function About() {
+  return (
+    <main className="max-w-4xl mx-auto px-4 py-12">
+      <h2 className="text-3xl font-bold mb-4">About Me</h2>
+      <p className=" text-lg text-gray-700">
+        This about page shares more information about my background and work.
+      </p>
+      <Skills
+        langages={["C#", "Python", "JavaScript", "HTML", "CSS"]}
+        contact="+242040492938"
+        education="I hold a Master’s degree and have completed a BYU Pathway bachelor’s certificate in software development, along with a Python certification. I am currently pursuing advanced studies in statistics, regression, and software development to strengthen both my theoretical knowledge and practical skills."
+      />
+    </main>
+  );
+}
